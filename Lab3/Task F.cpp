e920872049acc9923027bc3acf004c48c27f06bd
AlexGameTester <l.kirka@mail.ru>
2021-02-17 00:14:50 +0300

    Lab3 completed

---
+++ Lab3/Task F.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int *my_slightly_dumb_reallocation(int *source, unsigned int n_old, unsigned int n_new);
+
+int main()
+{
+    unsigned int n, i;
+    cin >> n;
+    int *a = my_slightly_dumb_reallocation(NULL, 0, n / 2);
+    for (i = 0; i < n / 2; i++)
+        cin >> a[i];
+    a = my_slightly_dumb_reallocation(a, n / 2, n);
+    for (; i < n; i++)
+        cin >> a[i];
+    int sum = 0;
+    for (i = 0; i < n; i++)
+        sum += a[i];
+    cout << sum << endl;
+    a = my_slightly_dumb_reallocation(a, n, n / 2);
+    a = my_slightly_dumb_reallocation(a, n / 2, 0);
+    a = my_slightly_dumb_reallocation(a, 0, 0);
+    return 0;
+}
+
+int *my_slightly_dumb_reallocation(int *source, unsigned int n_old, unsigned int n_new)
+{
+    if (n_new == 0)
+    {
+        if (source != NULL)
+        {
+            delete[] source;
+        }
+        return NULL;
+    }
+    int *new_arr = new int[n_new];
+    if (source != NULL)
+    {
+        unsigned int max_index = n_old > n_new ? n_new : n_old;
+        for (unsigned int i = 0; i < max_index; i++)
+        {
+            new_arr[i] = source[i];
+        }
+
+        delete[] source;
+    }
+
+    return new_arr;
+}
